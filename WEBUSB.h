4427fa2190d6ebd17918ec60d400f98cf71bc5f7
KBLiveSolutions <contact@kblivesolutions.com>
2022-03-11 13:39:54 +0100

    Update WEBUSB.h

--- WEBUSB.h
+++ WEBUSB.h
@@ -1,28 +1,36 @@
 
 
 Adafruit_USBD_WebUSB usb_web;
+long buffer_delay = 200;
+bool buffer_empty = HIGH;
+long buffer_time = 0;
+byte iii = 0;
 
 void  send_params(byte _key, byte layout_number, byte _num, byte _cc, byte _ch, byte _type) {
   byte buf[6] = {_key, layout_number,  _num,  _cc,  _ch, _type};
- 
+    buffer_empty = LOW;
     usb_web.write(buf, 6);
-   // Serial.flush();
- // }
+  pixels.setPixelColor(1, 0, 0, 100);
+  pixels.show();
 }
+
 
 void  test() {
 
   // for (byte layout_number = 0; layout_number < NUM_LAYOUT; layout_number++) {
-  for (byte i = 0; i < 12; i ++) {
+//  for (byte i = 0; i < 16; i ++)
+if (iii < 16){
+    buffer_time = millis();
     byte layout_number = 0;
-    byte _num = i;
+    byte _num = iii;
     byte _key = 10;
     byte _cc = b[0].short_control[layout_number];
     byte _ch = b[0].short_ch[layout_number];
     byte _type = b[0].short_type[layout_number];
     send_params(_key, layout_number, _num, _cc, _ch, _type);
+  iii ++;
   }  
-  for (byte i = 0; i < NUM_BUTTONS; i ++) {
+/*  for (byte i = 0; i < NUM_BUTTONS; i ++) {
     byte layout_number = 0;
     byte _num = i;
      byte _key = 12;
@@ -30,7 +38,7 @@ void  test() {
      byte _ch = b[i].long_ch[layout_number];
      byte _type = b[i].long_type[layout_number];
   //   send_params(_key, layout_number, _num, _cc, _ch, _type);
-  }
+  }*/
   //}
 }
 
@@ -57,11 +65,9 @@ void setup_webusb()
 
 
   // usb_web.setLandingPage(&landingPage);
-  usb_web.setLineStateCallback(line_state_callback);
+  // usb_web.setLineStateCallback(line_state_callback);
   //usb_web.setStringDescriptor("TinyUSB WebUSB");
   usb_web.begin();
-
-  Serial.begin(9600);
 
   // wait until device mounted
   while ( !TinyUSBDevice.mounted() ) delay(1);
@@ -71,5 +77,12 @@ void setup_webusb()
 
 void usbweb_loop()
 {
-  usb_web.flush();
+  if ( (millis()-buffer_time) > buffer_delay){
+  test();
+   // buffer_empty = HIGH; 
+  }  
+  if (usb_web.available() < 7) return;
+
+  uint8_t input[7];
+  usb_web.readBytes(input, 7);
 }
